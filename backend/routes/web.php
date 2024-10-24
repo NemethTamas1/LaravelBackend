0707538ebbfa0387253ded12664d54ef9b5caee8
NemethTamas1 <nemethtamas029@gmail.com>
2024-10-24 18:31:09 +0000

    homepage created

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [SiteController::class, "index"])->name("quote.index");
+Route::get('/', [SiteController::class, "index"])->name("home");
